999dadaf7a97a3d55632cc82293576fa0b4c3a55
eminred <eminsamedovred@gmail.com>
2023-10-22 13:38:29 +0300

    4.1 task

---
+++ src/ServiceGPS.php
@@ -0,0 +1,17 @@
+<?php
+class ServiceGPS implements ServiceInterface
+{
+  private $pricePerHour;
+  public $name = 'GPS';
+
+  public function __construct(int $pricePerHour)
+  {
+    $this->pricePerHour = $pricePerHour;
+  }
+
+  public function apply(TariffInterface $tariff, &$price) 
+  {
+    $hours = ceil($tariff->getMinutes() / 60);
+    $price += $this->pricePerHour * $hours;
+  }
+}
